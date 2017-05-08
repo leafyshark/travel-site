30a26ab490a898ead9ad6f597947fc5c9289568b
Taylor <taylor-lindores@hotmail.co.uk>
2017-05-08 19:07:34 +0100

    why do arrows keep appearing in these files

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,9 +4,6 @@ autoprefixer = require("autoprefixer"),
 nested = require('postcss-nested'),
 cssvars = require('postcss-simple-vars'),
 cssImport = require('postcss-import'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 mixins = require('postcss-mixins'),
 hexrgba = require('postcss-hexrgba');
 
@@ -14,37 +11,9 @@ gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
 	.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 });
-=======
-mixins = require('postcss-mixins');
->>>>>>> footer
-=======
-mixins = require('postcss-mixins');
->>>>>>> our-features
-=======
-mixins = require('postcss-mixins');
->>>>>>> testimonials
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
 	.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> our-features
-=======
->>>>>>> testimonials
-	.on('error', function(errorInfo) {
-		console.log(errorInfo.toString());
-		this.emit('end');
-	})
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> footer
-=======
->>>>>>> our-features
-=======
->>>>>>> testimonials
 	.pipe(gulp.dest('./app/temp/styles'))
 });
